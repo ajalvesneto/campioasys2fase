44d1cf97b1e4ce5289671367516e2b8df6e81c15
Antonio <antonio.janeto@gmail.com>
2021-05-19 21:39:58 -0300

    Layout register screen

---
+++ CampIoasys2Fase/Controller/RegisterScrollViewController.swift
@@ -0,0 +1,44 @@
+//
+//  RegisterScrollViewController.swift
+//  CampIoasys2Fase
+//
+//  Created by Antonio Alves on 19/05/21.
+//
+
+import UIKit
+
+class RegisterScrollViewController: UIViewController {
+    
+    
+    // MARK: IOBOutlets
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var phoneTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmpasswordTextField: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    @IBAction func registerButton(_ sender: Any) {
+    }
+    
+    
+
+}

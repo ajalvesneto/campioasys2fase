3fc06a86581687f5de403cfa9b7facd457ca2a2f
Antonio <antonio.janeto@gmail.com>
2021-05-25 10:47:51 -0300

    Refactoring code

---
+++ CampIoasys2Fase/Controller/ProfessionalsViewController2.swift
@@ -0,0 +1,8 @@
+//
+//  ProfessionalsViewController2.swift
+//  CampIoasys2Fase
+//
+//  Created by Antonio Alves on 24/05/21.
+//
+
+import Foundation
---
+++ CampIoasys2Fase/View/ProfessionalTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfessionalTableViewCell.swift
+//  CampIoasys2Fase
+//
+//  Created by Antonio Alves on 25/05/21.
+//
+
+import UIKit
+
+class ProfessionalTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
